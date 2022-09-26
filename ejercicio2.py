21f80bec306211ba94aa1a1a8df96f6f87731126
vale-herrero <vale4134@gmail.com>
2022-09-26 18:00:58 +0200

    ejercicio 2 completado

--- ejercicio2.py
+++ ejercicio2.py
@@ -1,1 +1,5 @@
-print("hola")
+numero_magico=12345679
+numero_usuario=int(input("ingrese un número entre 1 y 9: "))
+multiplicacion1 = numero_usuario*9
+multiplicacion2= multiplicacion1*numero_magico
+print(multiplicacion2)
