5ec9db6ca748261c6843219139a1f2f446d29313
vale-herrero <vale4134@gmail.com>
2022-09-26 18:10:20 +0200

    ejercicio 3 completado

---
+++ ejercicio3.py
@@ -0,0 +1,9 @@
+lista_1 = ["h",'o','l','a',' ', 'm','u','n','d','o']
+lista_2 = ["h",'o','l','a',' ', 'l','u','n','a']
+lista_3= []
+
+for letra in lista_1:
+    if (letra in lista_2) and (letra not in lista_3):
+        lista_3.append(letra)
+
+print(lista_3)
