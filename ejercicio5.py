a96004229dce8b2104984ca37ed75cfbafa88e66
vale-herrero <vale4134@gmail.com>
2022-09-29 12:39:15 +0200

    terminé ejercicio 5

--- ejercicio5.py
+++ ejercicio5.py
@@ -1,13 +1,12 @@
 import sys
 
 argumentos = sys.argv
-
 numero_argumentos=len(argumentos)
 
-if numero_argumentos==0:
+if numero_argumentos==1:
     print("ingrese un numero entero positivo")
 else: 
-    numero= argumentos[0]
+    numero= argumentos[1]
     unidades= '000'+numero[-1]
     decenas= '00'+numero[-2]+'0'
     centenas='0'+numero[-3]+'00'
